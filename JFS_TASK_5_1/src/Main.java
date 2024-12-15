3b4aa2531dfce149545f9ec45ac8669173ca75f9
AlexMelon <axrajan@gmail.com>
2024-12-15 11:28:23 +0530

    15.12.24

--- JFS_TASK_5_1/src/Main.java
+++ JFS_TASK_5_1/src/Main.java
@@ -1,7 +1,14 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        Stream<String> names = Stream.of("aBc", "d", "ef");
+        List<String> upperCaseNames = names.map(String::toUpperCase).collect(Collectors.toList());
+
+        System.out.println("UpperCaseNames are : " + upperCaseNames);
     }
 }
